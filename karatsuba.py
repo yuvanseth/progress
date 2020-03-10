3f643c85f10faa92f6d6d7c0d5dc70a3047e6b4a
yuvanseth <55777826+yuvanseth@users.noreply.github.com>
2020-03-11 01:52:51 +0530

    Update karatsuba.py

--- karatsuba.py
+++ karatsuba.py
@@ -1,16 +1,15 @@
-x = int(input("Enter first number: "))
-y = int(input("Enter second number: "))
-n = int(input("Enter number of digits: "))
+x = int(input("Enter number: "))
+y = int(input("Enter number: "))
 
-def prod(x,y,n):
+def prod(x,y):
     if len(str(x)) == 1 or len(str(y)) == 1:
         return x*y
     else:
-        n = max(len(str(x)),len(str(y)))
-        a = int(x/(10**(n/2)))
-        b = x % (10**(n/2))
-        c = int(y/(10**(n/2)))
-        d = y % (10**(n/2))
-        return (10**n * prod(a,c,n/2)) + (prod(b,c,n/2)) + (10**(n/2) (prod(a+b,c+d,n/2) - prod(a,c,n/2) - prod(b,c,n/2))) 
+        n = min(len(str(x)),len(str(y)))
+        a = x // (10**(n//2))
+        b = x % (10**(n//2))
+        c = y // (10**(n//2))
+        d = y % (10**(n//2))
+        return ((10**n) * (prod(a,c))) + (prod(b,c)) + ((10**(n//2)) * (prod(a+b,c+d) - prod(a,c) - prod(b,c))) 
 
-print(prod(x,y,n))
+print(prod(x,y))
