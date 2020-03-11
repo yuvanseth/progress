b2f6ef762a5678588b45b3998a9689aa600bbe23
yuvanseth <55777826+yuvanseth@users.noreply.github.com>
2020-03-12 01:34:02 +0530

    Create merge_sort+inv_count.py

---
+++ Algo1/merge_sort+inv_count.py
@@ -0,0 +1,36 @@
+def mergeSort(arr):
+    
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        l = arr[:mid]
+        r = arr[mid:]
+
+        mergeSort(l)
+        mergeSort(r)
+
+        i = j = k = 0
+
+        while i < len(l) and j < len(r):
+            if l[i] < r[j]:
+                arr[k] = l[i]
+                i+=1
+            else:
+                arr[k] = r[j]
+                j+=1
+            k+=1
+        
+        while i < len(l):
+            arr[k] = l[i]
+            i+=1
+            k+=1
+        
+        while j < len(r):
+            arr[k] = r[j]
+            j+=1
+            k+=1
+
+if __name__ == '__main__':
+
+    arr = [1,6,2,5,3,4]
+    mergeSort(arr)
+    print(arr)
